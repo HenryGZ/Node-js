e3e7a097e030d64fb1b930ba9a8413920eda2e7c
Henry Goes Zanin <henrygzanin@gmail.com>
2023-12-29 12:13:31 -0300

    feat - Add cors package for cross-origin resource sharing

--- todo list fullstack/backend/src/app.js
+++ todo list fullstack/backend/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const router = require('./router');
 
 const app = express();
 
 app.use(express.json());//aqui eu estou dizendo para o express que eu quero que ele use o json para fazer o parse do body da requisição
 app.use(router); //aqui eu estou dizendo para o express que eu quero que ele use o router para gerenciar as rotas da aplicação
+app.use(cors()); //aqui eu estou dizendo para o express que eu quero que ele use o cors para permitir que a aplicação seja acessada por qualquer endereço
 
 module.exports = app //é necessário para o teste exportar o app
